f87e6e3dd0c1e19d993f7802628b4b5c43a1687e
Ekaterina Menshenina <Ekaterina_Menshenina@epam.com>
2020-09-23 19:53:33 +0300

    fix imports

--- src/main/java/servlet/AccountServlet.java
+++ src/main/java/servlet/AccountServlet.java
@@ -5,7 +5,6 @@ import dto.AccountDto;
 import org.eclipse.jetty.http.HttpStatus;
 import services.AccountService;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
